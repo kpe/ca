9b3016afc05b61a79dbcd42d44dd3024eb98bf9d
kpe <kpe@users.noreply.github.com>
2022-04-08 13:30:34 +0200

    fixing renew for leaf certs

--- renew-cert.sh
+++ renew-cert.sh
@@ -5,10 +5,13 @@
 CERT_NAME=$1
 DAYS=${2:-365}
 
-if [[ -f ./$CERT_NAME.crt ]] ; then
+if [[ "$CERT_NAME" -eq "ca" ]] ; then
+    openssl x509 -x509toreq -in ca.crt -signkey ca.key -out ca.csr
+    openssl x509 -req -days $DAYS -in ca.csr -signkey ca.key -out ca.crt
+elif [[ -f ./$CERT_NAME.crt ]] ; then
     echo "renewing $CERT_NAME.crt for $DAYS days"
-    openssl x509 -x509toreq -in $CERT_NAME.crt -signkey $CERT_NAME.key -out $CERT_NAME.csr
-    openssl x509 -req -days $DAYS -in $CERT_NAME.csr -signkey $CERT_NAME.key -out $CERT_NAME.crt
+    openssl x509 -x509toreq -in $CERT_NAME.crt -key $CERT_NAME.key -out $CERT_NAME.csr
+    openssl x509 -req -days $DAYS -in $CERT_NAME.csr -key ca.key -out $CERT_NAME.crt
 else
     echo "Certificate NOT FOUND: $CERT_NAME.crt"
 fi
